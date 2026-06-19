259151b6189bc6c147950519b449e60a3559fd54
qubwk <qubel2221545ga@gmail.com>
2026-06-19 22:46:26 +0300

    Add internal packages cart and product with base functional

---
+++ internal/product/product.go
@@ -0,0 +1,36 @@
+package product
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID          uuid.UUID
+	Title       string
+	Description sql.NullString
+	Price       float64
+}
+
+func New(title string, description sql.NullString, price float64) *Product {
+	return &Product{
+		ID:          uuid.New(),
+		Title:       title,
+		Description: description,
+		Price:       price,
+	}
+}
+
+func (p *Product) Validate() error {
+	if p.Title == "" {
+		return fmt.Errorf("product title cannot be empty")
+	}
+
+	if p.Price < 0 {
+		return fmt.Errorf("product price cannot be lower than 0")
+	}
+
+	return nil
+}
